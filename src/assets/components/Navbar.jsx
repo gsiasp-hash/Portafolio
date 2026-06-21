595778be5ca5c6001e7cdbcc4c39b7d66260d8a2
Guillermo <g.siasp@gmail.com>
2026-06-20 22:11:03 -0400

    Add Tailwind css

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,9 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "../css/style.css";
 
 export default class Navbar extends Component {
   render() {
-    return (
-      <div>Navbar</div>
-    )
+    return <div className="bg-gray-800 text-white p-4 font-bold">Navbar</div>;
   }
 }
