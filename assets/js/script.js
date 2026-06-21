ffbdd50587a8faa9bd229ffb83928b71bca4c75b
Guillermo <g.siasp@gmail.com>
2026-06-20 21:42:13 -0400

    change proyecto creado con React

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".navbar-nav .nav-link").forEach((link) => {
-  link.addEventListener("click", () => {
-    const navbarCollapse = document.querySelector(".navbar-collapse");
-    const bsCollapse = bootstrap.Collapse.getInstance(navbarCollapse);
-    if (bsCollapse) {
-      bsCollapse.hide();
-    }
-  });
-});
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Home from "./assets/Views/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
