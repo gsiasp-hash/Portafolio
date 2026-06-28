f7ff6e175bd4a7c225ff8522c0a2909b2577adad
Guillermo <g.siasp@gmail.com>
2026-06-27 21:57:36 -0400

    change color de fondo

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from "./assets/components/Footer";
 function App() {
   return (
     <>
-      <div className="min-h-screen font-mono">
+      <div className="min-h-screen font-mono bg-[#020617]">
         <Navbar className="position-fixed z-0" />
         <Home />
         <Footer />
