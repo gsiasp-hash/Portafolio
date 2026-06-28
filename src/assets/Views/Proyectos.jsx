4b7564dde8f5e636d2b08182ff6f8b2ca1f4b1bc
Guillermo <g.siasp@gmail.com>
2026-06-27 22:29:04 -0400

    change marging seccion proyectos

--- src/assets/Views/Proyectos.jsx
+++ src/assets/Views/Proyectos.jsx
@@ -4,7 +4,7 @@ import Proyecto from "../components/Proyecto";
 export default class Proyectos extends Component {
   render() {
     return (
-      <div className="bg-[#020617] text-white py-10 px-3">
+      <div className="bg-[#020617] text-white py-10 px-3 md:mx-40">
         <h2 className="text-3xl pb-3">//Proyectos</h2>
         <Proyecto/>
       </div>
